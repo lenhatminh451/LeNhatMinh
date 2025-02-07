9fa248e2df8db1bc7ad695d543806ddf5ce48938
Nhat Minh Le <lenhatminh451@gmail.com>
2025-02-07 22:02:00 +0700

    feat: close dropdown when clicking outside

--- src/problem2/src/components/TokenSelect.tsx
+++ src/problem2/src/components/TokenSelect.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 
 interface TokenSelectProps {
     tokens: string[],
@@ -8,6 +8,7 @@ interface TokenSelectProps {
 
 const TokenSelect = ({ tokens, selectedToken, onChange }: TokenSelectProps) => {
     const [isOpen, setIsOpen] = useState<boolean>(false);
+    const dropdownRef = useRef<HTMLDivElement>(null);
 
     const getTokenIcon = (token: string) => {
         // Convert token name to match GitHub repo
@@ -28,8 +29,25 @@ const TokenSelect = ({ tokens, selectedToken, onChange }: TokenSelectProps) => {
         return `https://raw.githubusercontent.com/Switcheo/token-icons/refs/heads/main/tokens/${formattedToken}.svg`;
     }
 
+    // Close dropdown when clicking outside
+    useEffect(() => {
+        const handleClickOutside = (event: MouseEvent) => {
+            if(dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+                setIsOpen(false);
+            }
+        };
+
+        // Add event listener when the component is mounted
+        document.addEventListener("mousedown", handleClickOutside);
+
+        // Cleanup function
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        }
+    }, []);
+
     return (
-        <div className="custom-dropdown">
+        <div className="custom-dropdown"  ref={dropdownRef}>
             <div className="dropdown-header" onClick={() => setIsOpen(!isOpen)}>
                 <div className="dropdown-left">
                     <img src={getTokenIcon(selectedToken)} alt={selectedToken} className="token-icon"/>
